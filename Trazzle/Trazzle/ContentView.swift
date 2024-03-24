16738e6d196fd7946cec6ef96c38ec87734d3738
hongzuyeon <hongzuyeon@gmail.com>
2024-03-24 23:40:50 +0900

    프로젝트 파일 업로드, 최소타켓 ios 14.0 세팅

---
+++ Trazzle/Trazzle/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Trazzle
+//
+//  Created by 홍주연 on 2024/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trazzle/Trazzle/TrazzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrazzleApp.swift
+//  Trazzle
+//
+//  Created by 홍주연 on 2024/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TrazzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
