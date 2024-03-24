555aa3bc4389b56d4d6e2047762042de56d3a1a4
hongzuyeon <hongzuyeon@gmail.com>
2024-03-25 01:19:17 +0900

    탭바 생성, 뷰 분리해서 탭에 연결

---
+++ Trazzle/Trazzle/View/ExploreView.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreView.swift
+//  Trazzle
+//
+//  Created by 홍주연 on 2024/03/25.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text("ExploreView")
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
