17d4f2bad7100294c8eb9c784524b903eb13f6ae
hongzuyeon <hongzuyeon@gmail.com>
2024-03-26 22:58:02 +0900

    Floating Button 추가

--- Trazzle/Trazzle/View/HomeView/HomeView.swift
+++ Trazzle/Trazzle/View/HomeView/HomeView.swift
@@ -13,20 +13,39 @@ struct HomeView: View {
     var homeVM = HomeViewModel.shared
     
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            Canvas(rendersAsynchronously: true) { context, size in
-                let drawer = GeoJSONMapDrawer(featureCollection: homeVM.loadGeojson(),
-                                              colorDict: [
-                                                "RU": .mainGreen,
-                                              ])
-                drawer.drawMap(borderColor: .black,
-                               borderWidth: 1,
-                               size: size,
-                               context: context)
+        ZStack(alignment: .bottomTrailing) {
+            ScrollView(.horizontal,
+                       showsIndicators: false) {
+                Canvas(rendersAsynchronously: true) { context, size in
+                    let drawer = GeoJSONMapDrawer(featureCollection: homeVM.loadGeojson(),
+                                                  colorDict: [
+                                                    "RU": .mainGreen,
+                                                  ])
+                    drawer.drawMap(borderColor: .black,
+                                   borderWidth: 1,
+                                   size: size,
+                                   context: context)
+                }
+                // from a quick google, 1.65 is apparently best for a mercator map
+                .aspectRatio(1.65, contentMode: .fill)
             }
-            // from a quick google, 1.65 is apparently best for a mercator map
-            .aspectRatio(1.65, contentMode: .fill)
+            
+            // Floating Button
+            Button(action: {
+                print("floating button action")
+            }, label: {
+                Image(systemName: "plus") // 이미지 결정 시 추후 변경
+                    .resizable()
+                    .frame(width: 32, height: 32)
+                    .padding()
+            })
+            .background(Color.mainGreen)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .shadow(radius: 4, x: 0 , y: 4)
+            .offset(x: -16, y: -40)
         }
+        
     }
 }
 
