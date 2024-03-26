ba29349d9de17f1cbf214248d47a88d69d82aae4
joomin <jmnl225@github.com>
2024-03-26 19:40:52 +0900

    add LoginView / Kakao Login

--- Trazzle/Trazzle/TrazzleApp.swift
+++ Trazzle/Trazzle/TrazzleApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct TrazzleApp: App {
+    
+    init() {
+            // kakao SDK 초기화
+            KakaoSDK.initSDK(appKey: "a9cf757f00972eaeff4befd171abe6b1")
+        }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().onOpenURL(perform: { url in
+                if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                    AuthController.handleOpenUrl(url: url)
+                }
+            })
         }
     }
 }
---
+++ Trazzle/Trazzle/View/LoginView/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Trazzle
+//
+//  Created by joomin on 3/26/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+                    KakaoButton()
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
